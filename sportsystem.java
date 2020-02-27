f9bf1e761d0427e2436102ec2e69f6787acacea8
quiscobb <58868649+quiscobb@users.noreply.github.com>
2020-02-26 19:09:25 -0800

    Programming Fundamentals Exercise 2
    Programming Fundamentals Exercise 2

---
+++ sportsystem.java
@@ -0,0 +1,26 @@
+public class sportsystem{
+    public static void main(String args[]) {
+        
+        //create objects
+        //DataType nameObj = new Constructor();
+
+        player p1 = new Player("Lionel Messi",31,"Argentina", 1090900);
+        //p1.setName("Lionel Messi");
+        //p1.age = 31;
+        //p1.nationality = "Argentina";
+        //p1.salary = 1090900;
+        p1.printPlayer();
+    
+
+        // player p2 = new Player();
+        //p2.setName("Cristiano Ronaldo");
+        //p2.age = 34;
+        //p2.nationality = "Portugal";
+        //p2.salary = 1080900;
+        //p2.printPlayer();
+
+
+        //System.out.println(p1.name + " " + p1.age + " " + p1.nationality + " " + p1.salary);
+    }
+
+}
